babdb56f20b6b5803c49177c284d5dc26da3f375
matt wartell <matt.wartell@twosixtech.com>
2022-03-03 19:19:13 +0000

    skip docker image check until tagged

--- tests/unit/test_docker.py
+++ tests/unit/test_docker.py
@@ -1,6 +1,7 @@
 import pytest
 
-
+# TODO: what do we even want to check here?
+@pytest.mark.skip(reason="needs a mechanism to target scm tagged images")
 @pytest.mark.docker_required
 def test_one(docker_client):
     docker_client.images.list()
